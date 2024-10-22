7176428913f935ad095b3b15cf761ae7052dd24c
Dev-Kumbharvadia <devkumbharvadia@student.aau.in>
2024-10-22 15:27:56 +0530

    c

--- Backend/test1/Data/ApplicationDbContext.cs
+++ Backend/test1/Data/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using test1.Models;
+using test1.Models.Entity;
 
 namespace test1.Data
 {
@@ -13,6 +14,7 @@ namespace test1.Data
         public DbSet<Role> Roles { get; set; }
         public DbSet<UserRole> UserRoles { get; set; }
         public DbSet<TodoItem> TodoItems { get; set; }
+        public DbSet<LoginAuditTable> LoginAuditTables { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
@@ -21,6 +23,10 @@ namespace test1.Data
                 .HasKey(ur => new { ur.UserId, ur.RoleId });
 
             // Optionally, you can seed roles and users here if needed
+            modelBuilder.Entity<LoginAuditTable>()
+    .HasOne<User>() // Replace User with your related entity
+    .WithMany()
+    .HasForeignKey(a => a.UserId);
         }
     }
 }
